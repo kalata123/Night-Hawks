4ddbd33e42847add0478cd4cc901f103733ba497
kalata123 <kaloyan.angelov.tues@gmail.com>
2017-03-26 12:56:15 +0300

    Everything is Ready
    HAWKS' CIPHER

---
+++ Hawks' Cipher/Tues/apps/TuesConfig/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Test(models.Model):
+    Message = models.CharField(max_length=30, default="")
+    Keyword = models.CharField(max_length=30, default="")
+    Answer = models.CharField(max_length=30, default="",)
+
+# Create your models here.
